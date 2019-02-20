5b3759690b47ba203c328f2aff9378ca1fb8c5bf
xuzhihui <1042566734@qq.com>
2019-02-20 16:29:53 +0800

    对象的扩展

--- 对象的扩展.js
+++ 对象的扩展.js
@@ -1,4 +1,4 @@
-// 1.属性的简介表示
+// 1.属性的简写表示
 let a = 1
 let b = {a}
 console.log(b) // {a: 1}
@@ -7,3 +7,27 @@ function f(x, y) {
     return {x, y}
 }
 console.log(f(1, 2)) // {x: 1, y: 2}
+
+// 2.方法简写表示
+const o = {
+    method() {
+        return 'hello'
+    }
+    // method: function() {
+    //     return 'hello'
+    // }
+}
+
+// 3.属性吗表达式
+// let obj = {}
+// obj.foo = true
+// obj['a' + 'bc'] = 123
+// console.log(obj)
+
+let propKey = 'foo';
+
+let obj = {
+  [propKey]: true,
+  ['a' + 'bc']: 123
+};
+console.log(obj)
